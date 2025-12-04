76d6803f8fb7f84984b95d6249a83ccc8e1b50fd
Gerard Burgués Llavall <gerard.burgues@gerard-mac-mini.local>
2025-12-04 22:00:41 +0100

    Add docs folder for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/', // GitHub Pages repository base path
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
   }
 })
